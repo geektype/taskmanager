a5f871defce5a2cce45d883158deb63d62e98875
Abdullah <12223088+geektype@users.noreply.github.com>
2025-06-17 21:35:59 +0100

    Changes

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -12,10 +12,19 @@ class TaskManagerTests(unittest.TestCase):
     def setUpClass(cls):
         # Setup Chrome
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=420,420")
+        chrome_options.add_argument("--remote-debugging-pipe")
+        chrome_options.add_argument("--disable-software-rasterizer")
+
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        prefs = {
+            "profile.managed_default_content_settings.images": 2,
+            "profile.managed_default_content_settings.fonts": 2
+        }
+        chrome_options.add_experimental_option("prefs", prefs)
 
         cls.driver = webdriver.Chrome(options=chrome_options)
         cls.wait = WebDriverWait(cls.driver, 10)
