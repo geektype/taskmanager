fecf174609468aecba3e843b12f102c5c2309a3b
Abdullah <12223088+geektype@users.noreply.github.com>
2025-06-17 19:55:57 +0100

    Changes

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,14 @@
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 # Start Flask in the background
+echo 'Starting Flask'
 python app.py > flask.log 2>&1 &
 FLASK_PID=$!
+sleep 10
+cat flask.log
 
 # Wait for Flask to start
+echo 'Waiting for flask to start'
 for i in {1..30}; do
     if curl -s http://localhost:5000 > /dev/null; then
         break
@@ -19,14 +23,15 @@ for i in {1..30}; do
     fi
     sleep 1
 done
+echo 'Flask Started successfully'
 
 # Run tests
-curl -s -o /dev/null -w "%{http_code}\n" localhost:5000
-echo "Running Tests"
-python -m unittest tests/test_task_manager.py
-TEST_EXIT_CODE=$?
-
-# Kill Flask process
-kill $FLASK_PID
-
-exit $TEST_EXIT_CODE
+# curl -s -o /dev/null -w "%{http_code}\n" localhost:5000
+# echo "Running Tests"
+# python -m unittest tests/test_task_manager.py
+# TEST_EXIT_CODE=$?
+#
+# # Kill Flask process
+# kill $FLASK_PID
+#
+# exit $TEST_EXIT_CODE
